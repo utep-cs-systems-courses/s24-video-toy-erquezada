a330fa7685ae1333ab07565992fbe30008d12d0c
erquezada <erquezada@miners.utep.edu>
2024-04-20 18:31:29 -0600

    Modified lcd demos

--- lcdLib/lcddemo.c
+++ lcdLib/lcddemo.c
@@ -6,17 +6,40 @@
 #include "lcdutils.h"
 #include "lcddraw.h"
 
+drawLine()
+{
+  for (int i = 0; i < 128; i++)
+    drawPixel(i,i, COLOR_BLUE);
+}
+
+drawLine2() {
+  for (int i = 0; i < 30; i++)
+    drawPixel(i, 30-i, COLOR_PINK);
+}
+
+fillTriangle() {
+  for (int i = 0; i < 64; i++) {
+    for (int j = 0; j < i; j++) {
+      drawPixel(j, i, COLOR_BEIGE);
+    }
+  }
+}
+
 /** Initializes everything, clears the screen, draws "hello" and a square */
-int
-main()
+int main()
 {
   configureClocks();
   lcd_init();
   u_char width = screenWidth, height = screenHeight;
 
-  clearScreen(COLOR_BLUE);
+  clearScreen(BLACK);
 
-  drawString5x7(20,20, "hello", COLOR_GREEN, COLOR_RED);
+  drawString5x7(20,20, "hello, world", COLOR_GREEN, COLOR_RED);
+  drawString8X12(20,20, "Testing function", COLOR_GREEN, COLOR_RED);
+
+  drawLine();
+  drawLine2();
+  fillTriangle();
 
   fillRectangle(30,30, 60, 60, COLOR_ORANGE);
   
